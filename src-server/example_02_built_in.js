15df4031f18ca36a0f1ea0dc1045abf9292a05e8
Juan Prado <juan@juanpabloprado.com>
2020-02-16 22:25:34 -0600

    Use defer built in observable

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,4 +1,4 @@
-import { of, from, throwError, empty } from "rxjs";
+import { of, from, throwError, empty, defer } from "rxjs";
 import { createSubscriber } from "./lib/util";
 
 of(["this is an array", "hey"]).subscribe(createSubscriber("of"))
@@ -8,3 +8,13 @@ from([new Error("HEY")]).subscribe(createSubscriber("from"))
 throwError(214435).subscribe(createSubscriber("error"))
 
 empty().subscribe(createSubscriber("empty"))
+
+let sideEffect = 0;
+const defer$ = defer(() => {
+    sideEffect++;
+    return of(sideEffect);
+});
+
+defer$.subscribe(createSubscriber("defer$.one"));
+defer$.subscribe(createSubscriber("defer$.two"));
+defer$.subscribe(createSubscriber("defer$.three"));
