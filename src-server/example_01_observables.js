02cfed04115e6e743b38e73b6834dee2a8c1aef5
Juan Prado <juan@juanpabloprado.com>
2020-02-16 20:57:25 -0600

    Part 1

--- src-server/example_01_observables.js
+++ src-server/example_01_observables.js
@@ -1,6 +1,30 @@
-const promise = new Promise((resolve, reject) => {
-    console.log("IN PROMISE");
-    resolve("hey");
+import { Observable } from 'rxjs';
+
+const simple$ = new Observable(observer => {
+    console.log("Generating observable");
+    setTimeout(() => {
+        observer.next("An item!");
+        setTimeout(() => {
+            observer.next("Another item!");
+            observer.complete();
+        }, 1000);
+    }, 1000);
 });
 
-promise.then(console.log);
+const error$ = new Observable(observer => {
+    observer.error(new Error("STUFF"));
+});
+
+error$.subscribe(
+    item => console.log(`one.next ${item}`),
+    error => console.log(`one.error ${error.stack}`),
+    () => console.log("one.complete")
+);
+
+setTimeout(() => {
+    simple$.subscribe({
+        next: item => console.log(`two.next ${item}`),
+        error: error => console.log(`two.error ${error}`),
+        complete: () => console.log("two.complete")
+    })
+}, 3000);
