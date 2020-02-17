e3af788da1ba229c801835fd8b1ff761b899803c
Juan Prado <juan@juanpabloprado.com>
2020-02-16 19:24:25 -0600

    Polish reactive implementation

--- src-client/example_02_rxjs_ac.js
+++ src-client/example_02_rxjs_ac.js
@@ -6,17 +6,15 @@ import { map, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operato
 const $title = $("#title");
 const $results = $("#results");
 
-const keyUps$ = fromEvent($title, "keyup");
-const queries$ = keyUps$
+fromEvent($title, "keyup")
     .pipe(map(e => e.target.value))
     .pipe(distinctUntilChanged())
-    .pipe(debounceTime(250))
-    .pipe(switchMap(getItems)); // flatMapLatest
-
-queries$.subscribe(items => {
-    $results.empty();
-    $results.append(items.map(r => $(`<li />`).text(r)));
-});
+    .pipe(debounceTime(500))
+    .pipe(switchMap(getItems))
+    .subscribe(items => {
+        $results.empty();
+        $results.append(items.map(r => $(`<li />`).text(r)));
+    });
 
 // -----------------
 // Library
@@ -25,6 +23,6 @@ function getItems(title) {
     return new Promise((resolve, reject) => {
         window.setTimeout(() => {
             resolve([title, "Item 2", `Another ${Math.random()}`]);
-        }, 500 + (Math.random() * 5000));
+        }, 500 + (Math.random() * 2000));
     });
 }
