c45e3c90087f85c7b5cbf6723afb7b329d6ffeb9
Juan Prado <juan@juanpabloprado.com>
2020-02-16 21:49:07 -0600

    Use timer observable

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,7 +1,10 @@
-import { interval } from "rxjs";
+import { interval, timer } from "rxjs";
 import { take } from "rxjs/operators";
 import { createSubscriber } from "./lib/util";
 
 interval(500)
     .pipe(take(5))
-    .subscribe(createSubscriber("one"));
+    .subscribe(createSubscriber("interval"));
+
+timer(5000)
+    .subscribe(createSubscriber("timer"))
