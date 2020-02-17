4f34689a3316dc011d535b7e8626f05cecd42d00
Juan Prado <juan@juanpabloprado.com>
2020-02-16 22:17:11 -0600

    Use throwError built in observable

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,4 +1,4 @@
-import { of, from } from "rxjs";
+import { of, from, throwError } from "rxjs";
 import { createSubscriber } from "./lib/util";
 import { map } from "rxjs/operators";
 
@@ -6,3 +6,5 @@ of(["this is an array", "hey"]).subscribe(createSubscriber("of"))
 
 const arr = [1, 2, 3, 4, 5]
 from(arr).pipe(map(i => i * 5)).subscribe(createSubscriber("from"))
+
+throwError(new Error("HEY")).subscribe(createSubscriber("error"))
