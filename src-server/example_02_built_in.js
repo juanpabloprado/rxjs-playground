cfc9ae0362d4a7909bf7ef3f1e477a6c7fc450d9
Juan Prado <juan@juanpabloprado.com>
2020-02-16 22:18:58 -0600

    throw error will always throw an err

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -4,7 +4,7 @@ import { map } from "rxjs/operators";
 
 of(["this is an array", "hey"]).subscribe(createSubscriber("of"))
 
-const arr = [1, 2, 3, 4, 5]
-from(arr).pipe(map(i => i * 5)).subscribe(createSubscriber("from"))
 
-throwError(new Error("HEY")).subscribe(createSubscriber("error"))
+from([new Error("HEY")]).subscribe(createSubscriber("from"))
+
+throwError(214435).subscribe(createSubscriber("error"))
