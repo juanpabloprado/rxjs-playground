246c4e6bb8cfeb5738aa44f38268707985091436
Juan Prado <juan@juanpabloprado.com>
2020-02-17 00:11:37 -0600

    currentUser example

--- src-server/example_04_subjects.js
+++ src-server/example_04_subjects.js
@@ -1,23 +1,33 @@
 import { Subject, interval } from "rxjs";
 import { createSubscriber } from "./lib/util";
-import { take } from "rxjs/operators";
+import { take, map } from "rxjs/operators";
 
-const simple$ = new Subject();
+// const simple$ = new Subject();
 
-simple$.subscribe(createSubscriber("simple$"));
+// simple$.subscribe(createSubscriber("simple$"));
 
-simple$.next("Hello")
-simple$.next("WORLD")
-simple$.complete();
+// simple$.next("Hello")
+// simple$.next("WORLD")
+// simple$.complete();
 
-const interval$ = interval(1000).pipe(take(5));
-const intervalSubject$ = new Subject();
-interval$.subscribe(intervalSubject$);
+// const interval$ = interval(1000).pipe(take(5));
+// const intervalSubject$ = new Subject();
+// interval$.subscribe(intervalSubject$);
 
-intervalSubject$.subscribe(createSubscriber("sub1"));
-intervalSubject$.subscribe(createSubscriber("sub2"));
-intervalSubject$.subscribe(createSubscriber("sub3"));
+// intervalSubject$.subscribe(createSubscriber("sub1"));
+// intervalSubject$.subscribe(createSubscriber("sub2"));
+// intervalSubject$.subscribe(createSubscriber("sub3"));
 
+// setTimeout(() => {
+//     intervalSubject$.subscribe(createSubscriber("Look at me!"));
+// }, 3000)
+
+const currentUser$ = new Subject();
+const isLoggedIn$ = currentUser$.pipe(map(u => u.isLoggedIn));
+
+isLoggedIn$.subscribe(createSubscriber("isLoggedIn"));
+
+currentUser$.next({ isLoggedIn: false });
 setTimeout(() => {
-    intervalSubject$.subscribe(createSubscriber("Look at me!"));
-}, 3000)
+    currentUser$.next({ isLoggedIn: true, name: "juan" });
+}, 2000)
