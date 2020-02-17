edf6c3077a834297bd08977e36fbd62a7b147bec
Juan Prado <juan@juanpabloprado.com>
2020-02-16 22:20:25 -0600

    Use empty built in observable

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,10 +1,10 @@
-import { of, from, throwError } from "rxjs";
+import { of, from, throwError, empty } from "rxjs";
 import { createSubscriber } from "./lib/util";
-import { map } from "rxjs/operators";
 
 of(["this is an array", "hey"]).subscribe(createSubscriber("of"))
-
 
 from([new Error("HEY")]).subscribe(createSubscriber("from"))
 
 throwError(214435).subscribe(createSubscriber("error"))
+
+empty().subscribe(createSubscriber("empty"))
