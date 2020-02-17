39e672fdd7f6d211a18dff63c2241b5412dd45f3
Juan Prado <juan@juanpabloprado.com>
2020-02-16 22:14:52 -0600

    Perform transformations

--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,6 +1,8 @@
 import { of, from } from "rxjs";
 import { createSubscriber } from "./lib/util";
+import { map } from "rxjs/operators";
 
 of(["this is an array", "hey"]).subscribe(createSubscriber("of"))
 
-from([1, 32, 42, "HEY"]).subscribe(createSubscriber("from"))
+const arr = [1, 2, 3, 4, 5]
+from(arr).pipe(map(i => i * 5)).subscribe(createSubscriber("from"))
